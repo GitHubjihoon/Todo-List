9c49478110641bb233c383e45876e32b1ed78a07
KimJihoon <jihoon951118@gmail.com>
2021-08-08 15:33:03 +0900

    To do list 기능 구현

--- js/main.js
+++ js/main.js
@@ -20,15 +20,41 @@
         }
     ];
 
+
+    // 리스트 생성 이벤트 정리
     chamber[1].objs.textBtn.addEventListener("click", () => {
-        const makeList = document.createElement("li");
+        
+        const listContainer = document.createElement("li");
+        const makeList = document.createElement("p");
         const BTN = document.createElement('button');
 
+        // input 내용을  makeList 에 입력.
         makeList.innerText = chamber[1].objs.textInput.value;
-        chamber[2].objs.listBox.appendChild(makeList);
-        chamber[2].objs.listBox.appendChild(BTN);
+
+
+        chamber[2].objs.listBox.appendChild(listContainer);
+        listContainer.appendChild(makeList);
+        listContainer.appendChild(BTN);
 
         chamber[1].objs.textInput.value = "";
+        BTN.innerText = "X";
+
+        BTN.addEventListener("click", () => {
+            if (confirm("정말로 삭제 하모니카?")) {
+                chamber[2].objs.listBox.removeChild(listContainer);
+            } else {
+                return;
+            }
+        })
+
+        // 완료 리스트 확인 기능
+        makeList.addEventListener("click", () => {
+            if(makeList.classList.contains("selected")) {
+                makeList.classList.remove("selected");
+            } else {
+                makeList.classList.add("selected");
+            }
+        });
 
 
     })
